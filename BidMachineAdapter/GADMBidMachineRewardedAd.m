c022da5177a66c34c1e6cdf1309a9f9d8bcc0989
AssassinSC <ilia.log.1990@gmail.com>
2020-07-23 23:30:16 +0300

    update adapter

--- BidMachineAdapter/GADMBidMachineRewardedAd.m
+++ BidMachineAdapter/GADMBidMachineRewardedAd.m
@@ -6,24 +6,15 @@
 //  Copyright © 2019 bidmachine. All rights reserved.
 //
 
-#import "GADMBidMachineRewardedAd.h"
-#import "BMAFactory+BMRequest.h"
-#import "BMAFactory+RequestInfo.h"
-#import "BMANetworkExtras.h"
 #import "BMATransformer.h"
-#import "BMAConstants.h"
-#import "BMAUtils.h"
-#import "BMAError.h"
-
-#import <BidMachine/BidMachine.h>
-#import <StackFoundation/StackFoundation.h>
+#import "BMANetworkExtras.h"
+#import "GADMBidMachineRewardedAd.h"
+#import "BidMachineCustomEventRewardedAd.h"
 
 
-@interface GADMBidMachineRewardedAd () <BDMRewardedDelegate, GADMediationRewardedAd>
-
-@property (nonatomic, strong) BDMRewarded *rewardedAd;
-@property (nonatomic, copy) GADMediationRewardedLoadCompletionHandler originalComletion;
-@property (nonatomic, weak) id<GADMediationRewardedAdEventDelegate> delegate;
+@interface GADMBidMachineRewardedAd () {
+  BidMachineCustomEventRewardedAd *_rewardedAd;
+}
 
 @end
 
@@ -42,95 +33,15 @@
     return BMANetworkExtras.class;
 }
 
-- (GADMediationRewardedLoadCompletionHandler)loadingCompletion {
-    __weak typeof (self) weakSelf = self;
-    return ^id<GADMediationRewardedAdEventDelegate>(id<GADMediationRewardedAd> ad, NSError *error) {
-        id<GADMediationRewardedAdEventDelegate> delegate = STK_RUN_BLOCK(weakSelf.originalComletion, ad, error);
-        weakSelf.originalComletion = nil;
-        return delegate;
-    };;
++ (void)setUpWithConfiguration:(nonnull GADMediationServerConfiguration *)configuration
+             completionHandler:(nonnull GADMediationAdapterSetUpCompletionBlock)completionHandler {
+    completionHandler(nil);
 }
 
 - (void)loadRewardedAdForAdConfiguration:(GADMediationRewardedAdConfiguration *)adConfiguration
-                       completionHandler:
-                           (GADMediationRewardedLoadCompletionHandler)completionHandler {
-    self.originalComletion = completionHandler;
-    
-    NSDictionary *requestInfo = [[BMAFactory sharedFactory] requestInfoFromConfiguration:adConfiguration];
-    NSString *price = ANY(requestInfo).from(kBidMachinePrice).string;
-    
-    if (price) {
-        BDMRequest *auctionRequest = [BMAUtils.shared.fetcher requestForPrice:price type:BMAAdTypeRewarded];
-        if ([auctionRequest isKindOfClass:BDMRewardedRequest.self]) {
-           [self.rewardedAd populateWithRequest:(BDMRewardedRequest *)auctionRequest];
-        } else {
-            BMAError *error = [BMAError errorWithDescription:@"Bidmachine can't fint prebid request"];
-            self.delegate = self.loadingCompletion(nil, error);
-        }
-    } else {
-        __weak typeof(self) weakSelf = self;
-        [BMAUtils.shared initializeBidMachineWithRequestInfo:requestInfo completion:^(NSError *error) {
-            BDMRewardedRequest *auctionRequest = [[BMAFactory sharedFactory] rewardedRequestWithRequestInfo:requestInfo];
-            [weakSelf.rewardedAd populateWithRequest:auctionRequest];
-        }];
-    }
-}
-
-+ (NSString *)adapterVersion {
-    return @"1.5.0.0";
-}
-
-- (void)presentFromViewController:(UIViewController *)viewController {
-    if (self.rewardedAd.canShow) {
-        [self.rewardedAd presentFromRootViewController:viewController];
-    } {
-        BMAError *error = [BMAError errorWithDescription:@"BidMachine rewarded ad can't show ad"];
-        [self.delegate didFailToPresentWithError:error];
-    }
-}
-
-#pragma mark - Lazy
-
-- (BDMRewarded *)rewardedAd {
-    if (!_rewardedAd) {
-        _rewardedAd = [BDMRewarded new];
-        _rewardedAd.delegate = self;
-    }
-    return _rewardedAd;
-}
-
-#pragma mark - BDMRewardedDelegatge
-
-- (void)rewardedReadyToPresent:(BDMRewarded *)rewarded {
-    self.delegate = self.loadingCompletion(self, nil);
-}
-
-- (void)rewarded:(BDMRewarded *)rewarded failedWithError:(NSError *)error {
-    self.loadingCompletion(nil, error);
-}
-
-- (void)rewardedRecieveUserInteraction:(BDMRewarded *)rewarded {
-    [self.delegate reportClick];
-}
-
-- (void)rewarded:(BDMRewarded *)rewarded failedToPresentWithError:(NSError *)error {
-    [self.delegate didFailToPresentWithError:error];
-}
-
-- (void)rewardedWillPresent:(BDMRewarded *)rewarded {
-    [self.delegate willPresentFullScreenView];
-    [self.delegate didStartVideo];
-}
-
-- (void)rewardedDidDismiss:(BDMRewarded *)rewarded {
-    [self.delegate willDismissFullScreenView];
-    [self.delegate didEndVideo];
-    [self.delegate didDismissFullScreenView];
-}
-
-- (void)rewardedFinishRewardAction:(BDMRewarded *)rewarded {
-    GADAdReward *reward = [[GADAdReward alloc] initWithRewardType:@"" rewardAmount:NSDecimalNumber.zero];
-    [self.delegate didRewardUserWithReward:reward];
+                       completionHandler:(GADMediationRewardedLoadCompletionHandler)completionHandler {
+    _rewardedAd = [[BidMachineCustomEventRewardedAd alloc] initWithConfiguration:adConfiguration completionHandler:completionHandler];
+    [_rewardedAd loadRewardedVideo];
 }
 
 @end
